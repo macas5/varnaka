8a6ce01edc401a7fe067a46a9d78c780adddb49d
macas5 <mkalinauskas1993@gmail.com>
2021-02-07 14:25:21 +0000

    Added typescript and transformed files to .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Footer from './components/Footer/Footer'
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Showcase from './components/Showcase/Showcase';
 import NewItems from './components/NewItems/NewItems';
 import Instagram from './components/Instagram/Instagram';
+import './App.css';
 
 function App() {
   return (
