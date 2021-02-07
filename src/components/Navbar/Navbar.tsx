478fecdb3467eba9138ec5079d68bb3f72d7d7b4
macas5 <mkalinauskas1993@gmail.com>
2021-02-07 17:46:23 +0000

    Made progress on navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import './Navbar.css';
+
+import Logo from '../../Assets/Varnaka Logo T Rudas.png';
+import Search from '../../Assets/Search Logo.svg';
 
 const Navbar = () => {
   return(
-    <h2>Navbar</h2>
+    <nav className="flex justify-between items-center pv2 v-mid">
+      <div className="flex items-center">
+        <img src={Logo} alt="Varnaka" className="ph2"/>
+        <span className="ph2">CATEGORY</span>
+        <span className="ph2">CATEGORY</span>
+        <span className="ph2">CATEGORY</span>
+        <span className="shop">SHOP</span>
+      </div>
+      <div className="w-25 flex items-center justify-end">
+        <span className="search pa2 flex">
+          <span className="searchBox ph2 w5">Search</span>
+          <img src={Search} alt="Search"/>
+          </span>
+        <span className="ph2">LT</span>
+        <span className="ph2"><b>ENG</b></span>
+      </div>
+    </nav>
   )
 }
 
