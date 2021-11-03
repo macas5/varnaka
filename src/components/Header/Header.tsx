45c33ef43fb22b805150b154f08ca2b467a0845a
macas5 <mkalinauskas1993@gmail.com>
2021-11-03 16:01:42 +0000

    Modified fonts to match the wireframe

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return(
     <div className="ph5 tl headerbg">
       <section className="mw6 pb8">
-        <h2 className="f-4 pt6">Lorem ipsum dolor sit amet</h2>
+        <h2 className="f-4 pt6 normal">LOREM IPSUM DOLOR SIT AMET</h2>
         <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta praesentium dolorem voluptatibus ex labore iusto deleniti, exercitationem doloribus eos iste magnam molestias odit incidunt quaerat illum consequuntur quis totam?</p>
         <span className="cta ph4 pv2 mb7">CALL TO ACTION</span>
       </section>
