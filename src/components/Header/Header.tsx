d5d754b278ad96c98a4eb1499902e4052328c357
macas5 <mkalinauskas1993@gmail.com>
2021-11-03 15:11:38 +0000

    Entered basic header information

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
   return(
-    <h2>Header</h2>
+    <div className="ph5 tl headerbg">
+      <section className="mw6 pb7">
+        <h2 className="f-4 pt6">Lorem ipsum dolor sit amet</h2>
+        <p className="mb3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta praesentium dolorem voluptatibus ex labore iusto deleniti, exercitationem doloribus eos iste magnam molestias odit incidunt quaerat illum consequuntur quis totam?</p>
+        <span className="cta ph4 pv2">CALL TO ACTION</span>
+      </section>
+    </div>
   )
 }
-
 export default Header;
