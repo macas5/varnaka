07ff75f8c4f74ac2225ca89f1f065b2ed2f9ad5c
macas5 <mkalinauskas1993@gmail.com>
2021-11-03 15:40:54 +0000

    Clipped background image to polygon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,10 @@ import './header.css';
 const Header = () => {
   return(
     <div className="ph5 tl headerbg">
-      <section className="mw6 pb7">
+      <section className="mw6 pb8">
         <h2 className="f-4 pt6">Lorem ipsum dolor sit amet</h2>
-        <p className="mb3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta praesentium dolorem voluptatibus ex labore iusto deleniti, exercitationem doloribus eos iste magnam molestias odit incidunt quaerat illum consequuntur quis totam?</p>
-        <span className="cta ph4 pv2">CALL TO ACTION</span>
+        <p className="mb5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta praesentium dolorem voluptatibus ex labore iusto deleniti, exercitationem doloribus eos iste magnam molestias odit incidunt quaerat illum consequuntur quis totam?</p>
+        <span className="cta ph4 pv2 mb7">CALL TO ACTION</span>
       </section>
     </div>
   )
